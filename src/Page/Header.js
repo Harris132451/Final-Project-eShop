018cbbba5486db579342567bbd0a2bb02d3235ac
GGbondcoming <caicaidacc@gmail.com>
2024-04-08 13:35:19 +0800

    update

--- src/Page/Header.js
+++ src/Page/Header.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 import Weather from "./Component/Weather.js";
 import Inputbox from "./Component/input.js";
 import Cart from "./Component/SC.js";
+import { item } from "./Component/product";
+import { FaCaretDown } from "react-icons/fa";
+
 
 function Header(props) {
   const temperature = props.temperature;
@@ -12,6 +15,20 @@ function Header(props) {
   function ButtonLink({ to, BtnName }) {
     return <Link to={to}>{BtnName}</Link>;
   }
+
+  const dropdownNames = new Set(item.map(data => data.bigcategories));
+  const dropdownLinks = [...dropdownNames];
+
+  const uniqueSmallCategoriesMap = {};
+  dropdownLinks.forEach(bigcategorie => {
+    const smallCategoriesSet = new Set(
+      item
+        .filter(item => item.bigcategories === bigcategorie)
+        .map(filteredItem => filteredItem.smallcategories)
+    );
+    uniqueSmallCategoriesMap[bigcategorie] = Array.from(smallCategoriesSet);
+  });
+
   return (
     <>
       <div className="flex">
@@ -83,6 +100,30 @@ function Header(props) {
             />
           </div>
         )}
+      </div>
+
+      <div className=" shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40 flex justify-evenly">
+        {dropdownLinks.map(bigcategorie => (
+          <li key={bigcategorie} className="list-none group relative cursor-pointer">
+            <Link to={`/${bigcategorie}`} className="flex items-center gap-[2px] py-2">
+              {bigcategorie}
+              <span>
+                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+              </span>
+            </Link>
+            <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md">
+              <ul>
+                {uniqueSmallCategoriesMap[bigcategorie].map((smallCategory, index) => (
+                  <li key={index}>
+                    <Link to={`/${bigcategorie}/${smallCategory}`}>
+                      {smallCategory}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        ))}
       </div>
     </>
   );
